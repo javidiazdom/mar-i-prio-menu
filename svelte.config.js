203c6c5537050eb8853103a3c67684744bbf0b89
Javier Diaz Dominguez <javierdiazdominguez@Javiers-MacBook-Pro.local>
2025-11-20 22:37:02 +0000

    svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-        adapter: adapter({
-			fallback: '200.html', // may differ from host to host
-			pages: 'build',
-			assets: 'build',
-			precompress: false,
-			strict: true
+		adapter: adapter({
+			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/mar-i-prio-menu'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
-    }
+	}
 };
 
 export default config;
