65b4ea0ee04d67c6f440464f6824293e3dcb84b7
Javier Diaz Dominguez <javierdiazdominguez@Javiers-MacBook-Pro.local>
2025-11-20 21:53:23 +0000

    add build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { menu } from "../menu";
+    import logo from "$lib/assets/mar-i-prio-logo.png";
 
 	let categories: Array<Category> = menu.categories;
 	let selectedCategory: Category | undefined = $state(categories[0]);
@@ -9,16 +10,69 @@
     }
 </script>
 
-<nav>
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Cardo:ital,wght@0,400;0,700;1,400&family=MonteCarlo&display=swap" rel="stylesheet">
+</svelte:head>
+
+
+<div class="header">
+    <img alt="Mar i prio logo" src="{logo}" class="logo">
+</div>
+
+<nav class="navigation">
     {#each categories as category}
-        <button onclick={(e: Event) => setSelectedCategory(category.name)}>{category.name}</button>
+        <button class="montecarlo-regular" class:selected-button={selectedCategory?.name == category.name} onclick={(e: Event) => setSelectedCategory(category.name)}>{category.name}</button>
     {/each}
 </nav>
 
 <div>
-    <h1>{selectedCategory?.name}</h1>
+    <h1 class = "cardo-regular"> {selectedCategory?.name}</h1>
     {#each selectedCategory?.entries as entry}
-        <h2>{entry.title}</h2>
+        <h2 class = "cardo-regular">{entry.title}</h2>
         <p>{entry.description}</p>
     {/each}
 </div>
+
+
+
+<style>
+    .cardo-regular {
+        font-family: "Cardo", serif;
+        font-weight: 400;
+        font-style: normal;
+    }
+
+    .selected-button {
+        color: white;
+        background-color: aqua !important;
+    }
+
+    .montecarlo-regular {
+        font-family: "MonteCarlo", cursive;
+        font-weight: 400;
+        font-style: normal;
+    }
+
+    .header {
+        width: 100%;
+    }
+
+    .navigation {
+        width: 100%;
+        position: sticky;
+        display: flex;
+        height: 3rem;
+        button {
+            background-color: transparent;
+            border: none;
+            width: 100%;
+            border: 1px solid black
+        }
+    }
+
+    .logo {
+        height: 5rem;
+    }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+        adapter: adapter({
+			fallback: '200.html' // may differ from host to host
+		})
+    }
 };
 
 export default config;
